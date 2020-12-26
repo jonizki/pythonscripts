dcafc32636d01542f3e60dee31b655d95cb9b5a4
jonizki <59653825+jonizki@users.noreply.github.com>
2020-12-26 10:56:59 +0200

    Update Mousemove.py

--- Mousemove.py
+++ Mousemove.py
@@ -1,12 +1,30 @@
 import win32api
 import time
+import random
+import win32con
 
 def mouse_replay(x):
     with open(x, "r") as f:
         for line in f:
-            x = int(line.partition(",")[0])
-            y = int(line.partition(",")[2])
-            time.sleep(0.1)
-            win32api.SetCursorPos((x,y))
+            line = line.strip()
+            if line == "leftclick":
+                print("its left")
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+                sleeptime = random.uniform(0.001,0.01)
+                time.sleep(sleeptime)
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+            elif line == "rightclick":
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,0,0)
+                sleeptime = random.uniform(0.001,0.01)
+                time.sleep(sleeptime)
+                win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,0,0)     
+            else:
+                x = str(line.partition(",")[0])
+                y = str(line.partition(",")[2])
+                print(x,y)
+                sleeptime = random.uniform(0.000001, 0.000002)
+                time.sleep(sleeptime)
+                win32api.SetCursorPos((int(x),int(y)))
+            
 
 mouse_replay("mouse3.txt")
